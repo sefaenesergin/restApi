1e7f1b003ea3504bea01b632ad03e544514e5698
sefaenesergin <sefaenes07@icloud.com>
2023-03-11 01:30:05 +0300

    add model

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import config from 'config'
 import connect from './utils/connect';
-import logger from './utils/logger'
+import logger from './utils/logger' 
+import routes from './routes'
 
 const port = config.get<number>('port')
 
@@ -10,4 +11,6 @@ const app = express()
 app.listen(port,()=>{
     logger.info(`running at http://localhost:${port}`)
     connect();
+
+    routes(app)
 })
