c74a0327bf1941b2f664485ea769ee98f60349df
sefaenesergin <sefaenes07@icloud.com>
2023-03-13 02:20:11 +0300

    add user controller

---
+++ src/models/user.model.ts
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import config from "config";
+
+//typescript defination for schema
+export interface UserDocument extends mongoose.Document {
+  email: string;
+  name: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+  comparePassword(candidatePassword: string): Promise<Boolean>;
+}
+
+//schema definitation
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true, // it gives us a creator and updated date
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  let user = this as UserDocument;
+
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  //using this salt with bcrypt hash is the strongest password hashing algorithm
+  const salt = await bcrypt.genSalt(config.get<number>("saltWorkFactor"));
+
+  const hash = await bcrypt.hashSync(user.password, salt);
+
+  user.password = hash;
+
+  return next();
+});
+
+userSchema.methods.comparePassword = async function (
+  candidatePassword: string
+): Promise<boolean> {
+  const user = this as UserDocument;
+
+  return bcrypt.compare(candidatePassword, user.password).catch((e) => false);
+};
+
+//model definitation
+const UserModel = mongoose.model("User", userSchema);
+
+export default UserModel;
